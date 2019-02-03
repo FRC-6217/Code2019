fdec5136f129bfbaee6b760512e70ee2d8d93c31
stcoyle137 <22sc01@cf.k12.mn.us>
2019-02-02 20:53:53 -0600

    Initilize pixycam in the robot.java file

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -42,7 +42,8 @@ public class Robot extends TimedRobot {
     m_driveTrain = new DriveTrain();
 		m_pneumatics = new Pneumatics();
     m_oi = new OI();
-    
+    m_pixycam = new Pixycam();
+
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   }
