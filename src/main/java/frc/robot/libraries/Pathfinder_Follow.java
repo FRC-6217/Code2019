c1bbc172271c14b99e0049d83af98291b6eda208
stcoyle137 <22sc01@cf.k12.mn.us>
2019-02-17 13:47:12 -0600

    Sam's Pathfinder Code

--- src/main/java/frc/robot/libraries/Pathfinder_Follow.java
+++ src/main/java/frc/robot/libraries/Pathfinder_Follow.java
@@ -1,6 +1,7 @@
 package frc.robot.libraries;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Encoder;
 import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
@@ -12,7 +13,7 @@ public class Pathfinder_Follow{
     private double[] output = new double[4];
     private double[] desiredHeading = new double[4];
     private Trajectory[] WheelTraj;
-    private AnalogInput[] enc = {new AnalogInput(), new AnalogInput(), new AnalogInput(), new AnalogInput()};
+    private Encoder[] enc = {new Encoder(9,8), new Encoder(7,6), new Encoder(5,4), new Encoder(3,2)};
    
     private double wheel_diameter = 4 * 0.0254;
     private int pulsePerRev = 120;//ratio * pulse per revoletion
@@ -20,20 +21,18 @@ public class Pathfinder_Follow{
     private Pathfinder_Trajectory traj;
 
     public Pathfinder_Follow(Waypoint[] args){
-        setupFollow(args);
+        traj = new Pathfinder_Trajectory(args);
+        WheelTraj = traj.GetTraj();
     }
 
     public void setupFollow(Waypoint[] args){
-        traj = new Pathfinder_Trajectory(args);
-        WheelTraj = traj.GetTraj();
         for(int i = 0; i < 3; i++){
             WheelFollower[i] = new EncoderFollower(WheelTraj[i]);
-            WheelFollower[i].configureEncoder((int)(enc[i].getVoltage()), pulsePerRev, wheel_diameter);
+            WheelFollower[i].configureEncoder((int)(enc[i].get()), pulsePerRev, wheel_diameter);
             WheelFollower[i].configurePIDVA(1.0, 0.0, 0.0, 1 / max_velocity, 0);
             
-            output[i] = WheelFollower[i].calculate((int)(enc[i].getVoltage()));
+            output[i] = WheelFollower[i].calculate((int)(enc[i].get()));
             desiredHeading[i] = Pathfinder.boundHalfDegrees(Pathfinder.r2d(WheelFollower[i].getHeading())); // Bound to -180..180 degrees
-lCV 
         }
 
     }
