fe3b29c28a846e13629eef5fb7b0bb5ef417d2d5
stcoyle137 <22sc01@cf.k12.mn.us>
2019-02-28 20:41:52 -0600

    THis is not a empty commit message

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.commands.Grabbers;
+// import frc.robot.commands.Grabbers;
 
 public class Pneumatics extends Subsystem{
     private DoubleSolenoid sol1 = new DoubleSolenoid(1, 2);
@@ -12,7 +12,7 @@ public class Pneumatics extends Subsystem{
     
     @Override
     protected void initDefaultCommand() {
-        setDefaultCommand(new Grabbers());
+        // setDefaultCommand(new Grabbers());
     }
 
     public void runSolenoid(DoubleSolenoid.Value direction){
