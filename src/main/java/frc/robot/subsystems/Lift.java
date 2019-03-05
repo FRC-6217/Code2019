24e34fc0ec3532bd64fb8520afd291cf0b59b8ab
IsaiahW832 <isaiah68wildenberg@gmail.com>
2019-03-05 06:03:03 -0600

    Lift Subsystem

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -10,22 +10,32 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Add your docs here.
  */
 public class Lift extends PIDSubsystem {
-  /**
-   * Add your docs here.
-   */
-  public Lift(double setpoint) {
-    // Intert a subsystem name and PID values here
-    super("Lift", .5, .05, 0);
+  //Measures in Inches
+  private static final double MIN_HEIGHT = 16.75;
+  private static final double MAX_HEIGHT = 41.5;
+  public double SCALAR = 0.000671;
+  private static final double bOffset = 16.75;
+  private double upSpeed = 1;
+  private double downSpeed = .8;
 
-    setSetpoint(setpoint);
+  private Spark motor;
+  private Encoder encoder;
+
+  public Lift(int motorChannel, int encoderChannelA, int encoderChannelB) {
+    // Insert a subsystem name and PID values here
+    super("Lift", .5, .05, 0);
     
+    //initilize motor and encoder objects
+    motor = new Spark(motorChannel);
+    encoder = new Encoder(encoderChannelA, encoderChannelB);
+
+    //start pid loop
     enable();
   }
 
@@ -35,17 +45,55 @@ public class Lift extends PIDSubsystem {
     // setDefaultCommand(new MySpecialCommand());
   }
 
+  public void putSetpoint(double setpoint){
+    //Set out of range setpoints to be in range
+    if(setpoint > MAX_HEIGHT){
+      setpoint = MAX_HEIGHT;
+    }
+    else if(setpoint < MIN_HEIGHT){
+      setpoint = MIN_HEIGHT;
+    }
+
+    //print setpoint
+    SmartDashboard.putNumber("Lift Setpoint", setpoint);
+    
+    //set
+    setSetpoint(setpoint);
+  }
+
   @Override
   protected double returnPIDInput() {
-    // Return your input value for the PID loop
-    // e.g. a sensor, like a potentiometer:
-    // yourPot.getAverageVoltage() / kYourMaxVoltage;
-    return 0.0;
+    //calculate position in inches from encoder
+    double pos = encoder.get();
+    double position = ((pos * SCALAR) + bOffset);
+
+    //Prints encoder values
+    SmartDashboard.putNumber("Lift Raw Encoder", pos);
+    SmartDashboard.putNumber("Lift Encoder Position in.", position);
+    
+    return position;
   }
 
   @Override
   protected void usePIDOutput(double output) {
-    // Use output to drive your system, like a motor
-    // e.g. yourMotor.set(output);
+    //print motor speed and direction
+    SmartDashboard.putNumber("Lift Velocity", output);
+    motor.set(output);
+  }
+
+  public void up(){
+    motor.set(upSpeed);
+  }
+
+  public void down(){
+    motor.set(-downSpeed);
+  }
+
+  public void stop(){
+    motor.set(0);
+  }
+
+  public void resetEnc(){
+    encoder.reset();
   }
 }
